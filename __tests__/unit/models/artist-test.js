b24d43656e1167ea24ab62f9b1d9caf8ac2f8b93
Devjit Bedi <devjitbedi@Devjits-MacBook-Pro.local>
2019-04-02 19:39:03 -0700

    add some tests

---
+++ __tests__/unit/models/artist-test.js
@@ -0,0 +1,50 @@
+const { expect } = require('chai');
+const Artist = require('./../../../models/artist');
+
+describe('artist', () => {
+		describe('name', () => {
+
+				it('should be at least two characters', async () => {
+
+				try {
+				let artist = new Artist({name: 'a'});
+				await artist.validate();
+
+				} catch(error) {
+
+					expect(error.errors[0].message).to.equal('Name must be between 2 and 10 characters');
+				}
+
+				});
+
+
+
+				it('should be less than 10 characters', async () => {
+
+				try {
+				let artist = new Artist({name: 'abcdefghijkl'});
+				await artist.validate();
+
+				} catch(error) {
+
+					expect(error.errors[0].message).to.equal('Name must be between 2 and 10 characters');
+				}
+
+				});
+
+				it('should only contain letters', async () => {
+
+				try {
+				let artist = new Artist({name: '2'});
+				await artist.validate();
+
+				} catch(error) {
+
+					expect(error.errors[0].message).to.equal('Name must only contain letters');
+				}
+
+				});
+
+		});
+
+});
